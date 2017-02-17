37e596d2854fd2fce82f09245ccf7e9195d0735b
JEREMY ZABOROWSKI <jeremy@upnatomapps.com>
2017-02-17 01:44:51 -0500

    first user test is passing

--- we_know_api/test/models/user_test.rb
+++ we_know_api/test/models/user_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
-
 class UserTest < ActiveSupport::TestCase
+  test "user can be created with email address" do
+    email = FFaker::Internet.email
+    user = FactoryGirl.create(:user, email: email)
+    found = User.where({email: email})
+    assert user.email, found.first.email
+  end
   # test "the truth" do
   #   assert true
   # end
