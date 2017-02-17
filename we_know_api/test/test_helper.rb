1259584d1dd6839d976141552faeb08ddf58d2e8
Jeremy Zaborowski <jzlosman@gmail.com>
2017-02-17 10:05:42 -0500

    using Figaro instead of dotenv

--- we_know_api/test/test_helper.rb
+++ we_know_api/test/test_helper.rb
@@ -1,11 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'dotenv'
 
 class ActiveSupport::TestCase
-  Dotenv.load(File.expand_path("../../.ENV", __FILE__))
-
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
