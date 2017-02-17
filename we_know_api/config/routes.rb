99001cbc3fa3b2fc67a7f5da000cbf2a4c78c624
Jeremy Zaborowski <jzlosman@gmail.com>
2017-02-17 16:04:43 -0500

    Adding authentication token to user model

--- we_know_api/config/routes.rb
+++ we_know_api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   namespace :api, :defaults => {:format => :json} do
-    resources :users, :only => [:show, :create]
+    resources :users, :only => [:show, :create, :update, :destroy]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- we_know_api/test/controllers/api/users_controller_test.rb
+++ we_know_api/test/controllers/api/users_controller_test.rb
@@ -1,6 +1,7 @@
 require 'controllers/test_controller'
 
 class API::UsersControllerTest < TestController
+  # SHOW
   test "show returns a user" do
     user = FactoryGirl.create(:user)
     get :show, id: user.id, format: :json
@@ -8,6 +9,7 @@ class API::UsersControllerTest < TestController
     assert_equal user.id, result[:id]
   end
 
+  # CREATE
   test "when user is successfully created, render the user" do
     user = FactoryGirl.attributes_for :user
     post :create, { user: user }, format: :json
@@ -20,6 +22,30 @@ class API::UsersControllerTest < TestController
     post :create, { user: bad_user }, format: :json
 
     user_response = JSON.parse(response.body, symbolize_names: true)
-    assert_equal user_response[:errors][:email][0], "can't be blank"
+    assert_includes user_response[:errors][:email], "can't be blank"
+  end
+
+  # UPDATE
+  test "when successfully updating user" do
+    user = FactoryGirl.create :user
+    new_email = "new@email.com"
+    patch :update, { id: user.id, user: { email: new_email } }, format: :json
+    user_response = JSON.parse(response.body, symbolize_names: true)
+    assert_equal user_response[:email], new_email
+  end
+
+  test "when unsuccessfully updating user" do
+    user = FactoryGirl.create :user
+    new_email = "bademail"
+    patch :update, { id: user.id, user: { email: new_email } }, format: :json
+    user_response = JSON.parse(response.body, symbolize_names: true)
+    assert_includes user_response[:errors][:email], "is invalid"
+  end
+
+  # DELETE
+  test "when deleting user return a 204" do
+    user = FactoryGirl.create :user
+    delete :destroy, { id: user.id }, format: :json
+    assert_response 204
   end
 end
